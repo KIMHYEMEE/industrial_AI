aeecd5f7055460d905bfe53f58153c4ff52cbb58
Hyemee <53588762+MIA-khm@users.noreply.github.com>
2021-11-16 20:28:58 +0900

    Update concat.py

--- concat.py
+++ concat.py
@@ -17,24 +17,43 @@ os.chdir(path)
 
 file_list = os.listdir('./upzip_data')
 
+file_group = pd.DataFrame(file_list)
+file_group.columns = ['name']
+
+file_group = np.unique(file_group['name'].str[:9])
 
 # 2. Data concat
 
+data_group = {file: np.nan for file in file_group}
+
+col = pd.read_csv('./upzip_data/'+file_list[0]).columns
+
 for f in file_list:
+    print(f)
     tmp = pd.read_csv('./upzip_data/'+f)
     tmp = tmp.drop([0])
+    tmp.columns = col
     
     try:
-        df = pd.concat((df, tmp))
-    except:
-        df = tmp.copy()
         
+        data_group[f[:9]] = pd.concat((data_group[f[:9]], tmp))
+    except:
+        data_group[f[:9]] = tmp.copy()
+
+
+for f in file_group:
+    try:
+        df = pd.concat((df, data_group[f]))
+    except:
+        df = data_group[f].copy()
+    
+
         
 # 3. Data Formating
 df.index = range(np.shape(df)[0])
 
 df.TIME_STAMP = pd.to_datetime(df.TIME_STAMP , format='%Y-%m-%d %H:%M:%S')
-df.UTC = pd.to_datetime(df.UTC , format='%Y-%m-%d %H:%M:%S')
+#df.UTC = pd.to_datetime(df.UTC , format='%Y-%m-%d %H:%M:%S')
 
 # 4. Save data frame
 df.to_csv('data.csv',index = False)
